29b1415c59308412c07bf8e4e392caae613b62aa
AgustinCarcelen <agustin.carcelen@gmail.com>
2022-02-17 01:50:06 +0100

    up

--- Code/functions.py
+++ Code/functions.py
@@ -168,21 +168,140 @@ def logistic_regresion_train (X,y,control):
     ax[2].plot(fpr,tpr,label='roc model,auc='+str(auc))
     ax[2].legend(loc=4)
     plt.show()
-    return accuracy
+    return accuracy,auc
+
+#---------------------------------------------------------------------------------------------------
+##Normally X = X_full (data before train)
+##Normally y = Goal
+#control = (1, 0) if 1: change y column from Yes,No to 1,0
+#neighboors = number of neighbors
+#receives a database
+#shows accuracy of model, confusion matrix plot, roc_curve plot
+#returns accuracy,roc_score
+
+def knn_train (X,y,control,neighbors):
+    from sklearn.model_selection import train_test_split 
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.metrics import accuracy_score 
+    #split our data
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=40)
+    #apply and train logistic regresion
+    model=KNeighborsClassifier(n_neighbors = neighbors)
+    model.fit(X_train,y_train)
+    #calculate accuracy
+    y_test_pred=model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_test_pred)
+    print("Accuracy of LogisticRegresion:",accuracy)
+    
+    from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+    import matplotlib.pyplot as plt 
+    import seaborn as sns
+    from sklearn.metrics import roc_auc_score, roc_curve
+    #calculate confusion matrix and plot
+    fig, (ax) = plt.subplots(3,1, figsize=(15, 20))
+    cmx=confusion_matrix(y_test,y_test_pred)
+    disp=ConfusionMatrixDisplay(confusion_matrix=cmx)
+    disp.plot(ax=ax[0]);
+    #confusion matrix heatmap
+    sns.heatmap(cmx/np.sum(cmx), annot=True, fmt='.2%',cmap='Blues', ax=ax[1]);    
+    #transform y_test from Yes/No to 1/0 it is necessary
+    if control == True:
+        def yes_no_to_0_1(x):
+            if 'Yes' in x:
+                return 1
+            else: 
+                return 0
+        y_test = y_test.apply(yes_no_to_0_1)
+    else:
+        y_test = y_test
+    
+    y_pred_probs=model.predict_proba(X_test)[::,1]
+    #roc_curve plot
+    fpr,tpr, _ = roc_curve(y_test,y_pred_probs)
+    auc=roc_auc_score(y_test,y_pred_probs)
+    ax[2].plot(fpr,tpr,label='roc model,auc='+str(auc))
+    ax[2].legend(loc=4)
+    plt.show()
+    return accuracy, auc
+
+#---------------------------------------------------------------------------------------------------
+##Normally X = X_full (data before train)
+##Normally y = Goal
+#control = (1, 0) if 1: change y column from Yes,No to 1,0
+#receives a database
+#shows accuracy of model, confusion matrix plot, roc_curve plot
+#returns accuracy,roc_score
+
+def random_forest_train (X,y,control):
+    from sklearn.model_selection import train_test_split 
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.metrics import accuracy_score 
+    #split our data
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=40)
+    #apply and train logistic regresion
+    model=RandomForestClassifier(n_estimators=100)
+    model.fit(X_train,y_train)
+    #calculate accuracy
+    y_test_pred=model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_test_pred)
+    print("Accuracy of RandomForest:",accuracy)
+    
+    from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+    import matplotlib.pyplot as plt 
+    import seaborn as sns
+    from sklearn.metrics import roc_auc_score, roc_curve
+    #calculate confusion matrix and plot
+    fig, (ax) = plt.subplots(3,1, figsize=(15, 20))
+    cmx=confusion_matrix(y_test,y_test_pred)
+    disp=ConfusionMatrixDisplay(confusion_matrix=cmx)
+    disp.plot(ax=ax[0]);
+    #confusion matrix heatmap
+    sns.heatmap(cmx/np.sum(cmx), annot=True, fmt='.2%',cmap='Blues', ax=ax[1]);    
+    #transform y_test from Yes/No to 1/0 it is necessary
+    if control == True:
+        def yes_no_to_0_1(x):
+            if 'Yes' in x:
+                return 1
+            else: 
+                return 0
+        y_test = y_test.apply(yes_no_to_0_1)
+    else:
+        y_test = y_test
+    
+    y_pred_probs=model.predict_proba(X_test)[::,1]
+    #roc_curve plot
+    fpr,tpr, _ = roc_curve(y_test,y_pred_probs)
+    auc=roc_auc_score(y_test,y_pred_probs)
+    ax[2].plot(fpr,tpr,label='roc model,auc='+str(auc))
+    ax[2].legend(loc=4)
+    plt.show()
+    return model,accuracy,auc
+
+#---------------------------------------------------------------------------------------------------
+##Normally X = X_full (data before train)
+##Normally y = Goal
+#control = (1, 0) if 1: change y column from Yes,No to 1,0
+#receives a list database
+#shows accuracy, confusion matrix plot, roc_curve plot for each
+#returns a database with scaler,accuracy,roc_score
 
 def logistic_regresion_full (X,y,control):
     #we create a list with the names of the scaled values
     names = ['MinMaxScaler','MaxAbsScaler','StandardScaler','RobustScaler','Normalizer','QuantileTransformer','PowerTransformer']
     #we create a empty list to store accuracy of each scaled values
     accuracy = []
+    #we create a empty list to store score of each scaled values
+    score = []
     ##we create a empty list to store the results
-    result = pd.DataFrame(columns = ['Scaler','Accuracy'])
+    result = pd.DataFrame(columns = ['Scaler','Accuracy', 'Rock Auc Score'])
     #This for, call the function -logistic_regresion_train- to test each scaled value for the regression model
     for i in range(len(X)):  
         print("Numerical values sampled with: " + names[i])
         print(X[i])
-        accuracy.append(logistic_regresion_train (X[i],y,control))
-        result = result.append({'Scaler' : names[i] , 'Accuracy' : accuracy[i]}, ignore_index=True)
+        acu,roc = logistic_regresion_train (X[i],y,control)
+        accuracy.append(acu)
+        score.append(roc)
+        result = result.append({'Scaler' : names[i] , 'Accuracy' : accuracy[i], 'Rock Auc Score' : score[i]}, ignore_index=True)
         print("")
     return result
 
@@ -203,7 +322,7 @@ def yes_no_to_0_1 (data):
     else: 
         return 1
     
-#Recive a database and return all Value_counts()
+#Recive a database and show all Value_counts()
 def show_values (data):
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -216,7 +335,7 @@ def show_values (data):
         print("")
         print("")
         
-#Recive a database and return a correlation matrix plot
+#Recive a database and show a correlation matrix plot
 def matrix_correlation (data):
     import matplotlib.pyplot as plt
     import seaborn as sns
